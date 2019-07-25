bcd2dd438c21f14e0d181b1df17767e548b5f32d
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-07-25 17:41:44 +0200

    Jump
    Jump added

--- Handy Man/Assets/Scripts/MovementController.cs
+++ Handy Man/Assets/Scripts/MovementController.cs
@@ -6,10 +6,10 @@ public class MovementController : MonoBehaviour
 {
     Rigidbody2D rb;
     SpriteRenderer renderer;
-    //public float fsalto;
-    public float velocidad=1;
-    /*bool isGrounded;
-    public Transform groundcheck;
+    float velocity=1f,jumpForce=7f;
+    public bool ground;
+    private bool right;
+    /*public Transform groundcheck;
     public LayerMask suelo;
     public float radiogrounded;
     public bool left;*/
@@ -17,24 +17,29 @@ public class MovementController : MonoBehaviour
     //public bool salto;
     Animator anim;
 
-    // Start is called before the first frame update
+
     void Start()
     {
         renderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
-        //left = false;
         anim = GetComponent<Animator>();
-        //salto = false;
+        ground = true;
+        right = false;
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
-        if (Input.GetKey(KeyCode.A))
-        {            
-            if (rb.velocity.magnitude < 4)
+        /*if (ground==false)
+        {
+            rb.velocity -= new Vector2(0f, 1f * Time.timeScale);
+        }*/
+
+        if (Input.GetKey(KeyCode.A) && ground)
+        {
+            if (rb.velocity.x > -4)
             {
-                rb.velocity += new Vector2(-velocidad * Time.timeScale, 0f);
+                rb.velocity -= new Vector2(velocity * Time.timeScale, 0f);
             }
 
             if (anim.GetBool("Walk") == false)
@@ -42,26 +47,14 @@ public class MovementController : MonoBehaviour
                 anim.SetBool("Walk", true);
             }
             renderer.flipX = true;
-            
-            /*if (!left)
-            {
-                left = true;
-                facechange = true;
-                Vector3 childScale = child.transform.localScale;
-                childScale.x *= -1;
-                child.transform.localScale = childScale;
-                //print("girar left");
-            }
-            else
-                facechange = false;*/
-
+            right = false;
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && ground)
         {
-            if (rb.velocity.magnitude < 4)
+            if (rb.velocity.x < 4)
             {
-                rb.velocity += new Vector2(velocidad * Time.timeScale, 0f);
+                rb.velocity += new Vector2(velocity * Time.timeScale, 0f);
             }
 
             if (anim.GetBool("Walk") == false)
@@ -69,58 +62,33 @@ public class MovementController : MonoBehaviour
                 anim.SetBool("Walk", true);
             }
             renderer.flipX = false;
-
+            right = true;
         }
 
-        if (!Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.Space) && ground)
+        {
+            if (right)
+            {
+                rb.velocity = new Vector2(4f, jumpForce * Time.timeScale);
+            }
+            else
+            {
+                rb.velocity = new Vector2(-4f, jumpForce * Time.timeScale);
+            }
+            anim.SetBool("Walk", false);
+            anim.SetTrigger("Jump");
+            ground = false;            
+        }
+
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D) && ground)
         {
             rb.velocity = new Vector2(0f, 0f);
-            anim.SetBool("Walk", false);
         }
 
-        /*else if (Input.GetKey(KeyCode.D))
-        {
-            rb.velocity += new Vector2(velocidad, 0f);
-
-
-
-
-            if (anim.GetBool("Movimiento") == false)
-            {
-                anim.SetBool("Movimiento", true);
-            }
-
-            if (left)
-            {
-                left = false;
-                facechange = true;
-                //print("girar right");
-                Vector3 childScale = child.transform.localScale;
-                childScale.x *= -1;
-                child.transform.localScale = childScale;
-            }
-
-            else
-                facechange = false;
-        }*/
-
-        /*else if (!Input.GetKey(KeyCode.A) && (!Input.GetKey(KeyCode.D) && (rb.velocity.x <= 4f * velocidad) && (rb.velocity.x >= -4f * velocidad)))
-        {
-            anim.SetBool("Movimiento", false);
-        }*/
-
-        /*if (Input.GetKeyDown(KeyCode.W) && isGrounded)
-        {
-            salto = true;
-        }*/
-
-        /*if (facechange)
-        {
-            transform.Rotate(0f, 180f, 0f);
-            facechange = false;
-        }*/
-
-
+        if (rb.velocity.x == 0)
+        {           
+            anim.SetBool("Walk", false);
+        }
     }
 
     /*private void FixedUpdate()
@@ -132,10 +100,5 @@ public class MovementController : MonoBehaviour
             rb.AddForce(new Vector2(0, fsalto), ForceMode2D.Impulse);
             salto = false;
         }
-    }*/
-
-    /*public void Retroceso()
-    {
-        rb.AddForce(transform.right * -fuerzaretro);
     }*/
 }
