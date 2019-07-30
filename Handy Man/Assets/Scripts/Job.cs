44d5ad310fb5b5063ed1590bb0e38a08e2146e3f
Avilgor <35470345+Avilgor@users.noreply.github.com>
2019-07-30 15:57:12 +0200

    Job class
    Added job class with car name, reward and repair time

---
+++ Handy Man/Assets/Scripts/Job.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Job 
+{
+    private string Car;
+    private float time;
+    private int reward;
+    private System.Random rnd;
+
+    public Job(string name)
+    {
+        rnd = new System.Random();
+        Car = name;
+        time = rnd.Next(1,10) / 1000;
+        reward = 10 - ((int)time * 1000);
+        reward = (reward * 75);
+    }
+
+    public string getCar() { return Car; }
+    public float getTime() { return time; }
+    public int getReward() { return reward; }
+}
